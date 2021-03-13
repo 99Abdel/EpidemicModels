51b754917a98c120e056a258c7d85891d11870ca
99Abdel <a.msaad@studenti.unibs.it>
2021-03-13 15:33:28 +0100

    ode

--- GAmodels/I_GA.m
+++ GAmodels/I_GA.m
@@ -3,17 +3,17 @@ clear all
 % uncomment the data which you want to use.
 
 %% first  data curve
-xx = 1:1:27;
-data = xlsread('MyData.xlsx', 'Foglio1', 'B1:B28');
+t = 1:1:27;
+data = xlsread('MyData.xlsx', 'Foglio1', 'B1:B28'); %infected people per day
 
 %% second data curve 
-% xx = 1:1:55;
+% t = 1:1:55;
 % data = xlsread('MyData.xlsx', 'Foglio1', 'B211:B265');
 
 %% Loading data into arrays
 y = data(:,1); %copy data into array
-yy = y';
-I0 = yy(1);
+I = y';  % vector of infected people per day
+I0 = I(1);
 
 %% setting conditions for the ga
 A = []; 
@@ -24,17 +24,17 @@ lb = [0];
 ub = [1];
 
 %% defining the fitness function and the distance fitness function which will be inside the ga
-fun = @(a) (I0*exp(a(1)*xx));
-objFun = @(a) norm(fun(a)-yy);
+fun = @(a) (I0*exp(a(1)*t));
+objFun = @(a) norm(fun(a)-I);
 
 %% solution given by the ga 
 sol = ga(objFun, 1,A,b,Aeq,beq,lb,ub);
 
 %% graph of the solution compared withj the real one
 axes();
-plot(xx, yy, 'b+');
+plot(t, I, 'b+');
 hold on
-plot(xx, fun(sol), 'r-');
+plot(t, fun(sol), 'r-');
 legend({'Data points', 'Fitted Curve'})
 
 
---
+++ GAmodels/provaODE45.m
@@ -0,0 +1,54 @@
+clear all
+tmax = 30;
+N = 1e4;
+tspan = 0:0.0125:tmax;
+y0 = [N 1 0];
+[a,y] = ode45(@fun, tspan, y0);
+plot(a,y(:,2),'r-','Linewidth', 2)
+hold on
+
+%% extra to comapre with ODE
+
+
+dt = 0.0125;
+t = 0:dt:tmax;
+Nt = length(t);
+I0 = y0(2);
+I = zeros(1,Nt);
+R = zeros(1,Nt);
+b = .4;
+f = 1.8;
+S = zeros(1,Nt);
+I(1) = I0;
+
+for i = 1:Nt-1
+    
+%     I(i) = N*((I0*exp(b*t(i)))/(I0*exp(b*t(i)) + N - I0));
+%     S(i) = N-I(i);
+    S(i) = N-I(i)-R(i); %total susceptible people in this day
+    
+    dI = f*I(i)*S(i)/N -b*I(i);  % rate of change per day of infection(dI = dI/dt)
+    
+    I(i+1) = I(i) + dI*dt;  %total infected people in the day
+    
+    dR = b*I(i);  %rate of change per day of recovery ()
+    
+    R(i+1) = R(i) + dR*dt; %total removed people in the day
+    
+end 
+
+plot(t,I, 'b-')
+
+
+%% series of function for the ODE solver
+
+function dydt = fun(t,y)
+    N = 1e4;
+    dydt = zeros(3,1);
+    b = 1.8;
+    c = 0.4;
+    dydt(1) = -b*y(1)*y(2)/N; %-a*S*I
+    dydt(2) =  b*y(1)*y(2)/N - c*y(2); % a*S*I
+    dydt(3) =  c*y(2);        % b*I
+    
+end
